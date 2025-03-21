b8f73ee7c2c4635a70be20f115dbb7db581cb8fb
Emanuel Strobel <emanuel.strobel@gmail.com>
2025-03-21 14:27:05 +0100

    remove trash3

--- librecad/src/lib/scripting/rs_scriptlist.h
+++ librecad/src/lib/scripting/rs_scriptlist.h
@@ -7,7 +7,7 @@
 **
 **
 ** This file may be distributed and/or modified under the terms of the
-** GNU General Public License version 2 as published by the Free Software 
+** GNU General Public License version 2 as published by the Free Software
 ** Foundation and appearing in the file gpl-2.0.txt included in the
 ** packaging of this file.
 **
@@ -15,19 +15,17 @@
 ** but WITHOUT ANY WARRANTY; without even the implied warranty of
 ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ** GNU General Public License for more details.
-** 
+**
 ** You should have received a copy of the GNU General Public License
 ** along with this program; if not, write to the Free Software
 ** Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 **
-** This copyright notice MUST APPEAR in all copies of the script!  
+** This copyright notice MUST APPEAR in all copies of the script!
 **
 **********************************************************************/
 
 #ifndef RS_SCRIPTLIST_H
 #define RS_SCRIPTLIST_H
-
-#ifdef DEVELOPER
 
 #include<QList>
 #include "rs_script.h"
@@ -95,7 +93,5 @@ private:
     //! Currently active script
     //RS_Script* activeScript;
 };
-
-#endif // DEVELOPER
 
 #endif
